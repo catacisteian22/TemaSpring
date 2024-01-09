3a6caa6c0a417ed6a0e65c12d7755d5af113198d
Cătălina Cisteian <84501255+catacisteian22@users.noreply.github.com>
2024-01-09 16:22:20 +0200

    service und controller fur Bestellung

--- src/main/java/map/project/demo/buchFactory/RomanceBuchFactory.java
+++ src/main/java/map/project/demo/buchFactory/RomanceBuchFactory.java
@@ -6,7 +6,7 @@ import map.project.demo.model.Buch;
 public class RomanceBuchFactory implements  BuchFactory {
 
     @Override
-    public Buch createBuch(java.lang.Long idBuch, String title, String autor, int anzahlSeiten, int erstellungsjahr, float preis) {
+    public Buch createBuch(Long idBuch, String title, String autor, int anzahlSeiten, int erstellungsjahr, float preis) {
         return new Buch(idBuch, title, autor, "Romance", anzahlSeiten, erstellungsjahr, preis);
     }
 }
